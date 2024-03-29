6ff5abf42d1b853dde683fb32a26504016f1874a
Peter Lasne <plasne@iname.com>
2024-03-29 17:36:06 -0400

    improved prompts esp. for coherence

--- board-game-chat-eval/normalize.py
+++ board-game-chat-eval/normalize.py
@@ -2,12 +2,19 @@ from promptflow import tool
 import json
 
 
+def extract_json(payload: str) -> str:
+    start = payload.find("{")
+    end = payload.rfind("}") + 1
+    return payload[start:end]
+
+
 @tool
 def normalize(groundedness: str, relevancy: str, coherance: str) -> str:
-    jgrd = json.loads(groundedness)
-    jrel = json.loads(relevancy)
+    jgrd = json.loads(extract_json(groundedness))
+    jrel = json.loads(extract_json(relevancy))
+    jcoh = json.loads(extract_json(coherance))
     return {
         "groundedness": jgrd["score"],
         "relevance": jrel["score"],
-        "coherance": int(coherance),
+        "coherance": jcoh["score"],
     }
--- evaluations/eval.sh
+++ evaluations/eval.sh
@@ -1,10 +1,22 @@
-# run the inference flow
-INFERENCE_OUTPUT=$(pf run create --flow ./board-game-chat/flow.dag.yaml --data ./evaluations/questions.jsonl --column-mapping chat_history='${data.chat_history}' query='${data.query}' mode='${data.mode}')
-INFERENCE_ID=$(echo "$INFERENCE_OUTPUT" | grep -oP '"name": "\K[^"]+')
+# get params
+NUM_RUNS=${1:-1} # default to 1 run
+EVAL_SET=$2
+EVAL_DESC=$3
 
-# run the eval flow
-EVAL_OUTPUT=$(pf run create --flow ./board-game-chat-eval/flow.dag.yaml --data ./evaluations/questions.jsonl --column-mapping question='${data.query}' context='${run.outputs.context}' answer='${run.outputs.answer}' --run $INFERENCE_ID)
-EVAL_ID=$(echo "$EVAL_OUTPUT" | grep -oP '"name": "\K[^"]+')
 
-# output the eval flow details
-pf run show-details -n $EVAL_ID
+# run the script for the specified number of times
+for ((i=1; i<=NUM_RUNS; i++))
+do
+
+    # run the inference flow
+    INFERENCE_OUTPUT=$(pf run create --flow ./board-game-chat/flow.dag.yaml --data ./evaluations/questions.jsonl --column-mapping chat_history='${data.chat_history}' query='${data.query}' mode='${data.mode}')
+    INFERENCE_ID=$(echo "$INFERENCE_OUTPUT" | grep -oP '"name": "\K[^"]+')
+
+    # run the eval flow
+    EVAL_OUTPUT=$(pf run create --flow ./board-game-chat-eval/flow.dag.yaml --data ./evaluations/questions.jsonl --column-mapping question='${data.query}' context='${run.outputs.context}' answer='${run.outputs.answer}' eval_set="'$2'" eval_desc="'$3'" --run $INFERENCE_ID)
+    EVAL_ID=$(echo "$EVAL_OUTPUT" | grep -oP '"name": "\K[^"]+')
+
+    # output the eval flow details
+    pf run show-details -n $EVAL_ID
+
+done
