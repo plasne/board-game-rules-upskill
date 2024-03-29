cfbf85db8db3d56d09dff60a0c0bd77a7b87ee58
Peter Lasne <plasne@iname.com>
2024-03-29 13:21:02 -0400

    finished eval

--- evaluations/eval.sh
+++ evaluations/eval.sh
@@ -3,7 +3,7 @@ INFERENCE_OUTPUT=$(pf run create --flow ./board-game-chat/flow.dag.yaml --data .
 INFERENCE_ID=$(echo "$INFERENCE_OUTPUT" | grep -oP '"name": "\K[^"]+')
 
 # run the eval flow
-EVAL_OUTPUT=(pf run create --flow ./board-game-chat-eval/flow.dag.yaml --data ./evaluations/questions.jsonl --column-mapping question='${data.query}' context='no-context' answer='${run.outputs.answer}' --run $INFERENCE_ID)
+EVAL_OUTPUT=$(pf run create --flow ./board-game-chat-eval/flow.dag.yaml --data ./evaluations/questions.jsonl --column-mapping question='${data.query}' context='${run.outputs.context}' answer='${run.outputs.answer}' --run $INFERENCE_ID)
 EVAL_ID=$(echo "$EVAL_OUTPUT" | grep -oP '"name": "\K[^"]+')
 
 # output the eval flow details
